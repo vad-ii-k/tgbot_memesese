1ed251c4c79b6b4c6ddd3a4011e3cb43bee2fc55
vad-ii-k <sazanov7615@gmail.com>
2022-08-24 20:11:23 +0300

    Init commit

---
+++ tgbot/handlers/echo.py
@@ -0,0 +1,28 @@
+from aiogram import types, Router, F
+from aiogram.fsm.context import FSMContext
+from aiogram.utils.markdown import hcode, code
+
+echo_router = Router()
+
+
+@echo_router.message(F.text, state=None)
+async def bot_echo(message: types.Message):
+    text = [
+        "Эхо без состояния.",
+        "Сообщение:",
+        message.text
+    ]
+
+    await message.answer('\n'.join(text))
+
+
+@echo_router.message(F.text)
+async def bot_echo_all(message: types.Message, state: FSMContext):
+    state_name = await state.get_state()
+    text = [
+        f'Эхо в состоянии {code(state_name)}',
+        'Содержание сообщения:',
+        hcode(message.text)
+    ]
+    await message.answer('\n'.join(text))
+
