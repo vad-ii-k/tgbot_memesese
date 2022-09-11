3eb61c55db6f8c19839dc0659f7c27c023e35a7a
vad-ii-k <sazanov7615@gmail.com>
2022-09-11 12:26:17 +0300

    Update: pylint settings

--- tgbot/keyboards/inline.py
+++ tgbot/keyboards/inline.py
@@ -4,5 +4,6 @@ from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 def get_keyboard_with_nums(num_of_buttons: int) -> InlineKeyboardMarkup:
     builder = InlineKeyboardBuilder()
-    [builder.add(InlineKeyboardButton(text=str(num), callback_data=str(num))) for num in range(1, num_of_buttons + 1)]
+    for num in range(1, num_of_buttons + 1):
+        builder.add(InlineKeyboardButton(text=str(num), callback_data=str(num)))
     return builder.as_markup(resize_keyboard=True, one_time_keyboard=True)
